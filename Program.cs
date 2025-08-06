bb22522adc8970c2fd9e57006f131fe834b2749d
tatojastine-n <jajanicoletato@gmail.com>
2025-08-06 11:16:09 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhoAreYou
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+
+                Console.Write("Enter first name: ");
+                firstName = Console.ReadLine().Trim();
+
+                Console.Write("Enter last name: ");
+                lastName = Console.ReadLine().Trim();
+
+                Console.WriteLine($"\nYour full name is: {firstName} {lastName}");
+        }
+    }
+}
